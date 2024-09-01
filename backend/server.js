17f74ea25e5981bd116cbae997d53f0bdface330
Bharti-28 <patilbharti959@gmail.com>
2024-09-01 10:02:28 +0530

    creating frontend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const{chats} = require("./data/data");
 
 const app = express(); //instance of express
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is Running");
+    res.send("API is Running successfully");
 });
 
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
 //help of the app variable we can start our own server now
-app.listen(5000,console.log("Server has started....."));
+app.listen(5000,console.log(`Server has started on PORT ${PORT}`));
 
 
 //to check the server run command-> node file name
